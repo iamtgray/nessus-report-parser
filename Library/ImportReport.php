10093d01b3337061a95e874b64822773b4072adb
simonbt <simon.beattie@randomstorm.com>
2014-04-15 15:11:07 +0100

    Re-added ImportReport.php contents

--- Library/ImportReport.php
+++ Library/ImportReport.php
@@ -1,85 +1,129 @@
 <?php
 /**
- * ReportGenerator -- ImportAbstract.php
+ * ReportGenerator -- ImportReport.php
  * User: Simon Beattie
  * Date: 14/04/2014
- * Time: 10:18
+ * Time: 10:22
  */
 
 namespace Library;
 
+class ImportReport extends \Library\ImportAbstract
+{
 
-class ImportAbstract {
+    protected $xmlObj;
+    protected $reportName;
+    protected $reportID;
 
-    /**
-     * @var PDO $pdo
-     */
-    protected $pdo;
-
-    /**
-     * @var array $config
-     */
-    protected $config;
-
-    public function __construct($pdo, array $config)
+    public function createReport($xml)
     {
-        $this->setPdo($pdo);
-        $this->setConfig($config);
-    }
-
-    /**
-     * setConfig sets the config property in object storage
-     *
-     * @param array $config
-     * @throws InvalidArgumentException
-     * @return StatusAbstract
-     */
-    public function setConfig($config)
-    {
-        if (empty($config))
-        {
-            throw new \InvalidArgumentException(__METHOD__ . ' cannot accept an empty config');
+        $this->xmlObj = simplexml_load_file($xml);
+        $this->reportName = $this->xmlObj->Report[0]['name'] . PHP_EOL;
+        $createReport = $this->getPdo()->prepare('INSERT INTO reports (report_name, created) VALUES(?, ?)');
+        $createdOk = $createReport->execute(array($this->xmlObj->Report[0]['name'], date('Y-m-d H:i:s')));
+        if (!$createdOk) {
+            die('Sorry, we couldn\'t create the new report: ' . $createReport->errorInfo()[2] . PHP_EOL);
         }
-        $this->config = $config;
-        return $this;
+
+        $this->reportID = $this->getPdo()->lastInsertId();
+        $this->createHost();
+
+        return array('reportName' => $this->reportName);
     }
 
-    /**
-     * getConfig returns the config from the object
-     *
-     * @return array
-     */
-    public function getConfig()
+    public function completeReport()
     {
-        return $this->config;
-    }
-
-    /**
-     * setPdo sets the pdo property in object storage
-     *
-     * @param \PDO $pdo
-     * @throws InvalidArgumentException
-     * @return StatusAbstract
-     */
-    public function setPdo($pdo)
-    {
-        if (empty($pdo))
-        {
-            throw new \InvalidArgumentException(__METHOD__ . ' cannot accept an empty pdo');
+        $completedInsert = $this->getPdo()->prepare('UPDATE reports SET imported = 1 WHERE report_id = ?');
+        $reportCompleted = $completedInsert->execute(array('1'));
+        if (!$reportCompleted) {
+            die('Sorry, we couldn\'t complete the report insert: ' . $completedInsert->errorInfo()[2] . PHP_EOL);
         }
-        $this->pdo = $pdo;
-        return $this;
+
+        return $this->getPdo()->lastInsertId();
     }
 
-    /**
-     * getPdo returns the pdo from the object
-     *
-     * @return \PDO
-     */
-    public function getPdo()
+    private function createHost()
     {
-        return $this->pdo;
+        $count = 1;
+        $insertHost = $this->getPdo()->prepare('INSERT INTO hosts (report_id, host_name) VALUES(?, ?)');
+        foreach ($this->xmlObj->Report[0]->ReportHost as $num => $host) {
+            echo 'Importing host ' . $count . ' of ' . $this->xmlObj->Report[0]->ReportHost->count() . ' ... ';
+            $insertedHost = $insertHost->execute(array($this->reportID, $host['name']));
+            if (!$insertedHost) {
+                die('Sorry, we couldn\'t insert the host: ' . $insertHost->errorInfo()[2] . PHP_EOL);
+            }
+
+            $hostID = $this->getPdo()->lastInsertId();
+            $properties = $host[0]->HostProperties->children(); /* @var SimpleXMLElement $properties */
+
+            $this->addHostDetails($hostID, $properties);
+
+            $this->addVulnerability($host, $hostID);
+
+            $count++;
+        }
     }
 
+    private function addHostDetails($hostID, $properties)
+    {
+        foreach ($properties as $tagItem) /* @var SimpleXMLElement $tagItem */ {
 
-} 
+            $names = array('mac-address', 'system-type', 'operating-system', 'host-ip', 'host-fqdn', 'netbios-name');
+
+            $attribs = $tagItem->attributes();
+            $name = $attribs['name'];
+            $value = (string)$tagItem;
+            $hostUpdate = $this->getPdo()->prepare('UPDATE hosts SET ' . str_replace('-', '_', $name) . '=? WHERE id=?');
+
+            if (in_array($name, $names)) {
+                $updateHost = $hostUpdate->execute(array($value, $hostID));
+                if (!$updateHost) {
+                    die('Sorry, we couldn\'t update the host: ' . $hostUpdate->errorInfo()[2] . PHP_EOL);
+                }
+            }
+
+        }
+    }
+
+    private function addVulnerability($host, $hostID)
+    {
+        $foundVulnerabilities = array();
+
+        foreach ($host->ReportItem as $item) /* @var SimpleXMLElement $item */ {
+            $attributes = array();
+
+
+            $addVuln = $this->getPdo()->prepare('INSERT INTO vulnerabilities (pluginID, vulnerability, svc_name, severity, pluginFamily) VALUES(?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE pluginID=PluginID');
+            $addVulnLink = $this->getPdo()->prepare('INSERT INTO host_vuln_link (report_id, host_id, plugin_id, port, protocol) VALUES(?, ?, ?, ?, ?)');
+
+            foreach ($item->attributes() as $attribute => $value) {
+
+                if ($attribute != 'pluginName') {
+                    $attributes[$attribute] = (string)$value;
+                }
+
+            }
+
+            $vulnAdded = $addVuln->execute(array($attributes['pluginID'], $item['pluginName'], $attributes['svc_name'], $attributes['severity'], $attributes['pluginFamily']));
+            if (!$vulnAdded) {
+                die('Sorry, we couldn\'t add the vulnerability: ' . $addVuln->errorInfo()[2] . PHP_EOL);
+            }
+
+
+
+            $vulnLinkAdded = $addVulnLink->execute(array($this->reportID, $hostID, $attributes['pluginID'], $attributes['port'],  $attributes['protocol'],));
+            if (!$vulnLinkAdded) {
+                die('Sorry, we couldn\'t add the vulnerability link: ' . $addVulnLink->errorInfo()[2] . PHP_EOL);
+            }
+
+            $foundVulnerabilities[$attributes['pluginID']] = (string)$item['pluginName'];
+
+        }
+
+
+        #print_r($foundVulnerabilities);
+        echo "Found " . count($foundVulnerabilities) . " vulnerabilities" . PHP_EOL;
+
+    }
+
+}
